01559514f9183712f276c8714aa7c51a38d15e4f
miszelo <michal.kawczak@gmail.com>
2023-10-26 16:50:49 +0200

    added main component

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,12 +1,12 @@
 import { Footer } from "./Footer.tsx";
 import { Header } from "./Header.tsx";
-import { Main } from "./styled/Main.ts";
+import { Main } from "./Main.tsx";
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Main></Main>
+      <Main />
       <Footer />
     </>
   );
