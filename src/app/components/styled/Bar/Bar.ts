fddcab3de38b223f8dd55b043d1175ea67e9484d
MikosJ <56031072+MikosJ@users.noreply.github.com>
2023-10-31 00:11:59 +0100

    logo fix

--- src/app/components/styled/Bar/Bar.ts
+++ src/app/components/styled/Bar/Bar.ts
@@ -6,6 +6,7 @@ export const Bar = styled.div<{ $header?: boolean }>`
   justify-content: space-around;
   gap: 20vw;
   height: fit-content;
+  min-height: ${(props) => (props.$header ? "10rem":"5rem")};
   background-color: ${(props) => (props.$header ? "#27ecc5" : "#0da684")};
   background-image: ${(props) => (props.$header ? "linear-gradient(#0da684,#27ecc5)":"linear-gradient(#27ecc5,#0da684)")};
 `;
