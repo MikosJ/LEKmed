c722824916d31949950f27ccecf5a11ef83fddfa
miszelo <michal.kawczak@gmail.com>
2023-11-02 14:14:11 +0100

    rename logo file

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,17 @@
-import {Title} from "./styled/Bar/Title.ts";
-import {Bar} from "./styled/Bar/Bar.ts";
-import {BarSection} from "./styled/Bar/BarSection.ts";
-import {Logo} from "./styled/Bar/Logo.ts";
+import { Title } from "./styled/Bar/Title.ts";
+import { Bar } from "./styled/Bar/Bar.ts";
+import { BarSection } from "./styled/Bar/BarSection.ts";
+import { Logo } from "./styled/Bar/Logo.ts";
 
 export const Header = () => {
-    return (
-        <Bar $header={true}>
-            <Logo src={"../svg/logo2.svg"}></Logo>
-            <BarSection>
-                <Title>Lekarze</Title>
-                <Title>O nas</Title>
-                <Title>Kontakt</Title>
-            </BarSection>
-        </Bar>
-    );
+  return (
+    <Bar $header={true}>
+      <Logo src={"../svg/logo.svg"}></Logo>
+      <BarSection>
+        <Title>Lekarze</Title>
+        <Title>O nas</Title>
+        <Title>Kontakt</Title>
+      </BarSection>
+    </Bar>
+  );
 };
