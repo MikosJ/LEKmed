9c8642a849567de5600369e8cdc09b8e5ead0321
miszelo <michal.kawczak@gmail.com>
2023-11-02 18:00:56 +0100

    implement music on PageNotFound.tsx

--- src/app/pages/PageNotFound.tsx
+++ src/app/pages/PageNotFound.tsx
@@ -3,10 +3,10 @@ import { MainContent } from "../components/styled/MainContent.ts";
 export const PageNotFound = () => {
   return (
     <>
+      <audio autoPlay loop>
+        <source src="/kociak_muzyka.mp3" type="audio/mpeg" />
+      </audio>
       <MainContent $direction={"column"}>
-        <audio id={"audio"} loop autoPlay>
-          <source src={"/kociak.mp3"} type={"audio/mpeg"} />
-        </audio>
         <a>Ups...</a>
         <a>Taka strona nie istnieje</a>
         <img src={"../gifs/kociak.gif"} alt={"brak kociaka"} />
