6d4c1934c3f6305e2f5493a738335fad075a8046
miszelo <michal.kawczak@gmail.com>
2023-11-02 16:53:32 +0100

    check if no data was passed to Reservation.tsx

--- src/app/components/styled/MainContent.ts
+++ src/app/components/styled/MainContent.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const MainContent = styled.div`
   display: flex;
+  justify-content: center;
   gap: 1rem;
   margin: 10vw;
 `;
--- src/app/pages/Reservation.tsx
+++ src/app/pages/Reservation.tsx
@@ -3,15 +3,19 @@ import { useLocation } from "react-router-dom";
 
 export const ReservationPage = () => {
   const location = useLocation();
-  console.log(location);
+  if (location.state === null) {
+    return (
+      <MainContent>
+        <div>Brak danych</div>
+      </MainContent>
+    );
+  }
   return (
     <MainContent>
-      <h1>Rezerwacja</h1>
-      <a>{location.state.firstname}</a>
-      <a>{location.state.lastname}</a>
-      <a>{location.state.title}</a>
-      <img src={location.state.image} alt={"brak zdjecia"} />
-      <a>test</a>
+      <a>{location?.state.firstname}</a>
+      <a>{location?.state.lastname}</a>
+      <a>{location?.state.title}</a>
+      <img src={location?.state.image} alt={"brak zdjecia"} />
     </MainContent>
   );
 };
