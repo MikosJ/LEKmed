ea846a9d9a22c09e406716294d70f750d0a793ec
miszelo <michal.kawczak@gmail.com>
2023-11-02 21:39:59 +0100

    refactor PageNotFound.tsx

--- src/app/pages/PageNotFound.tsx
+++ src/app/pages/PageNotFound.tsx
@@ -1,33 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { MainContent } from "../components/styled/MainContent.ts";
 
-export const PageNotFound: React.FC = () => {
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-
-  useEffect(() => {
-    if (audioRef.current) {
-      // Ręczne rozpoczęcie odtwarzania muzyki po zamontowaniu komponentu
-      audioRef.current.volume = 0.5;
-      audioRef.current.loop = true;
-      audioRef.current.play().catch((error) => {
-        // Obsługa błędów podczas próby odtworzenia muzyki
-        console.error("Błąd podczas odtwarzania muzyki:", error);
-      });
-    }
-
-    // Czyszczenie muzyki po odmontowaniu komponentu
-    return () => {
-      if (audioRef.current) {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-      }
-    };
-  }, []);
-
+export const PageNotFound = () => {
   return (
     <>
       <MainContent $direction={"column"}>
-        <audio ref={audioRef} autoPlay loop>
+        <audio autoPlay loop>
           <source src="/kociak_muzyka.mp3" type="audio/mpeg" />
         </audio>
         <a>Ups...</a>
