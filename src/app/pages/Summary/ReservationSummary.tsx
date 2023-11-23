19a45388369f3134aff1a8417ab56a4dffccc67d
miszelo <michal.kawczak@gmail.com>
2023-11-23 12:39:37 +0100

    styling

--- src/app/pages/Summary/ReservationSummary.tsx
+++ src/app/pages/Summary/ReservationSummary.tsx
@@ -2,6 +2,8 @@ import { MainContent } from "../../styledComponents/MainContent/MainContent.ts";
 import { Location, useLocation } from "react-router-dom";
 import { Reservation } from "../../types/types.ts";
 import {
+  Second,
+  First,
   Block,
   Button,
   Key,
@@ -23,10 +25,10 @@ export const ReservationSummary = () => {
 
   const [title, setTtile] = useState("Twoja wizyta została zarezerwowana!");
 
-  const [button, setButton] = useState("Anuluj Wizyte");
+  const [button, setButton] = useState("Anuluj wizyte");
 
   function handleClick() {
-    if (button === "Anuluj Wizyte") {
+    if (button === "Anuluj wizyte") {
       setVisitStatus("Anulowana");
       setTtile("Twoja wizyta została anulowana");
       setButton("Zarezerwuj");
@@ -34,7 +36,7 @@ export const ReservationSummary = () => {
     }
     setVisitStatus("Zarezerwowana");
     setTtile("Twoja wizyta została zarezerwowana!");
-    setButton("Anuluj Wizyte");
+    setButton("Anuluj wizyte");
   }
 
   return (
@@ -42,43 +44,53 @@ export const ReservationSummary = () => {
       <ReservationContainer>
         <Title>{title}</Title>
         <Summary>
-          <Block>
-            <Key>Pacjent: </Key>
-            <Value>
-              {location.state.patient.firstname}{" "}
-              {location.state.patient.lastname}
-            </Value>
-          </Block>
-          <Block>
-            <Key>Doktor: </Key>
-            <Value>
-              {location.state.doctor.firstname} {location.state.doctor.lastname}
-            </Value>
-          </Block>
-          <Block>
-            <Key>Rodzaj wizyty: </Key>
-            <Value>{location.state.visit}</Value>
-          </Block>
-          <Block>
-            <Key>Data: </Key>
-            <Value>
-              {location.state.pickedTime.date.toLocaleDateString()}{" "}
-              {location.state.pickedTime.hour}
-            </Value>
-          </Block>
-          <Block>
-            <Key>Cena: </Key>
-            <Value>
-              {location.state.price.value} {location.state.price.currency}
-            </Value>
-          </Block>
-          <Block>
-            <Key>Status: </Key>
-            <Value>{visitStatus}</Value>
-          </Block>
+          <First>
+            <Block>
+              <Key>Pacjent: </Key>
+              <Value>
+                {location.state.patient.firstname}{" "}
+                {location.state.patient.lastname}
+              </Value>
+            </Block>
+            <Block>
+              <Key>Doktor: </Key>
+              <Value>
+                {location.state.doctor.firstname}{" "}
+                {location.state.doctor.lastname}
+              </Value>
+            </Block>
+          </First>
+          <Second>
+            <Block>
+              <Key>Rodzaj wizyty: </Key>
+              <Value>{location.state.visit}</Value>
+            </Block>
+            <Block>
+              <Key>Data: </Key>
+              <Value>
+                {location.state.pickedTime.date.toLocaleDateString()}{" "}
+                {location.state.pickedTime.hour}
+              </Value>
+            </Block>
+            <Block>
+              <Key>Cena: </Key>
+              <Value>
+                {location.state.price.value} {location.state.price.currency}
+              </Value>
+            </Block>
+            <Block>
+              <Key>Status: </Key>
+              <Value>{visitStatus}</Value>
+            </Block>
+          </Second>
         </Summary>
       </ReservationContainer>
-      <Button onClick={handleClick}>{button}</Button>
+      <Button
+        reservation={visitStatus === "Zarezerwowana"}
+        onClick={handleClick}
+      >
+        {button}
+      </Button>
     </MainContent>
   );
 };
--- src/app/pages/Summary/SummaryStyles.ts
+++ src/app/pages/Summary/SummaryStyles.ts
@@ -1,27 +1,48 @@
 import styled from "styled-components";
-
+export const First = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
+`;
+export const Second = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
 export const Block = styled.div`
   display: flex;
   justify-content: space-between;
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 export const Key = styled.a`
   text-align: left;
+  @media (max-width: 1000px) {
+    text-align: left;
+  }
 `;
 export const Value = styled.a`
   font-weight: bold;
   text-align: right;
-`;
-export const Summary = styled.div`
-  display: grid;
-  min-width: 70%;
-  column-gap: 1rem;
-  grid-template-rows: 1fr 1fr;
-  grid-template-columns: 1fr;
 
   @media (max-width: 1000px) {
-    grid-template-rows: 1fr 1fr 1fr;
-    grid-template-columns: 1fr;
-    font-size: 40px;
+    text-align: left;
+  }
+`;
+export const Summary = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  @media (max-width: 1000px) {
+    flex-direction: column;
   }
 `;
 export const Title = styled.h2`
@@ -33,7 +54,8 @@ export const ReservationContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 50%;
+  width: 50vw;
+  height: 30vh;
   background: #e9fffd;
   border-radius: 10px;
   border: solid rgba(0, 0, 0, 0.49) 5px;
@@ -42,14 +64,14 @@ export const ReservationContainer = styled.div`
   font-size: 1rem;
 
   @media (max-width: 1000px) {
-    min-height: 30vh;
-    justify-content: center;
+    width: 50vw;
+    height: 30vh;
     font-size: 30px;
   }
 `;
 
-export const Button = styled.button`
-  background: #d92222;
+export const Button = styled.button<{ reservation?: boolean }>`
+  background: ${(props) => (props.reservation ? "red" : "green")};
   color: #ffffff;
   border: none;
   padding: 1.5vw 2vh;
@@ -57,9 +79,4 @@ export const Button = styled.button`
   cursor: pointer;
   font-size: 1.4rem;
   transition: background 0.3s ease;
-  font-weight: 501;
-
-  &:hover {
-    background: rgba(108, 22, 41, 0.5);
-  }
 `;
